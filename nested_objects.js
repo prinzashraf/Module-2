c04f4ac6f3f2a2eeca1f8d2766e04104bb1be068
prinzashraf <ns2757613@gmail.com>
2023-09-11 12:56:08 +0530

    main

--- nested_objects.js
+++ nested_objects.js
@@ -1,17 +1,14 @@
-//let us create an object
-let person =
+let person = 
 {
-    name:"ashraf",
-    age:19 ,
-    gender:"male",
-    eyecolor:"black" ,
+    name:"Ragini",
+    age:13,
+    gender:"Female",
+    eyecolor:"black",
     cars:{
-        car1:"honda" ,
+        car1:"Honda",
         car2:"BMW"
     }
-
-    }
-   for (i in person.cars)
-   {
+}
+for (i in person.cars){
     console.log(person.cars[i]);
-   }
+}
